a86694786f3bee18586eb6da8206ac1b1060306d
jameshoo123 <hoosinyong@hotmail.com>
2022-03-29 13:46:35 +0800

    feat: add edit and delete function

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -12,4 +12,18 @@ router.post('/new', (req, res) => {
   res.redirect('/')
 })
 
+router.get('/:expense_id/edit', async (req, res) => {
+  const expense_id = req.params.expense_id
+  const expense = await Expense.findById({ _id: expense_id }).lean()
+  // console.log(expense)
+  res.render('edit', { expense })
+})
+
+router.post('/:expense_id/delete', async (req, res) => {
+  const expense_id = req.params.expense_id
+  await Expense.findOneAndDelete({ _id: expense_id })
+  // console.log(expense)
+  res.redirect('/')
+})
+
 module.exports = router
