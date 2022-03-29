04e4673cad543868cd61932b982c8dea991c8cce
jameshoo123 <hoosinyong@hotmail.com>
2022-03-29 10:45:29 +0800

    feat: data render in home.js

--- models/seeds/expenseSeeds.js
+++ models/seeds/expenseSeeds.js
@@ -5,20 +5,26 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const db = require('../../config/mongoose')
+const res = require('express/lib/response')
 
 const CATEGORY = {
+  //<i class="fa-solid fa-house"></i>
   家居物業: 'https://fontawesome.com/icons/home?style=solid',
+  //<i class="fa-solid fa-van-shuttle"></i>
   交通出行: 'https://fontawesome.com/icons/shuttle-van?style=solid',
+  //<i class="fa-solid fa-face-grin-beam"></i>
   休閒娛樂: 'https://fontawesome.com/icons/grin-beam?style=solid',
+  //<i class="fa-solid fa-utensils"></i>
   餐飲食品: 'https://fontawesome.com/icons/utensils?style=solid',
+  //<i class="fa-solid fa-pen"></i>
   其他: 'https://fontawesome.com/icons/pen?style=solid',
 }
 
 db.once('open', async () => {
   await Promise.all(
-    expenses.map(expense => {
+    expenses.map(async expense => {
       const { name, date, amount } = expense
-      Expense.create({ name, date, amount })
+      const expenses = await Expense.create({ name, date, amount })
     })
   )
   console.log('expenses created !')
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const Expense = require('../../models/expense')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  const expenses = await Expense.find().lean()
+  console.log(expenses)
+  res.render('index', { expenses })
 })
 
 module.exports = router
