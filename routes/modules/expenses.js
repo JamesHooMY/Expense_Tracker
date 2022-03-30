8bfbcff45d5c4372440ab858471efea852234a26
jameshoo123 <hoosinyong@hotmail.com>
2022-03-30 14:07:40 +0800

    feat: modify sorting function and add totalAmount calculation

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -1,35 +1,51 @@
 const express = require('express')
 const router = express.Router()
 const Expense = require('../../models/expense')
+const Category = require('../../models/category')
 
-router.get('/new', (req, res) => {
-  res.render('new')
+router.get('/new', async (req, res) => {
+  const categories = await Category.find().lean()
+  res.render('new', { categories })
 })
 
 router.post('/new', (req, res) => {
+  const userId = req.user._id
+  // 這裏的 userId 為 object 但不需要用 .toString() 因爲後續沒有要做邏輯判斷，只要放入 DB 或在 DB 找資料的話，可以直接使用 object
+  // console.log(userId)
   const expense = req.body
+  expense.userId = userId
   Expense.create(expense)
   res.redirect('/')
 })
 
 router.get('/:expense_id/edit', async (req, res) => {
+  const userId = req.user._id
   const expense_id = req.params.expense_id
-  const expense = await Expense.findOne({ _id: expense_id }).lean()
+  const expense = await Expense.findOne({ _id: expense_id, userId }).lean()
+  const categories = await Category.find().lean()
+  const { categoryId } = expense
+  categories.forEach(category => {
+    if (category._id.toString() === categoryId) {
+      category.selected = 'selected'
+    }
+  })
   // console.log(expense)
-  res.render('edit', { expense })
+  res.render('edit', { expense, categories })
 })
 
 // here
 router.post('/:expense_id/edit', async (req, res) => {
+  const userId = req.user._id
   const expense_id = req.params.expense_id
   const expense = req.body
-  await Expense.findOneAndUpdate({ _id: expense_id }, expense)
+  await Expense.findOneAndUpdate({ _id: expense_id, userId }, expense)
   res.redirect('/')
 })
 
 router.post('/:expense_id/delete', async (req, res) => {
+  const userId = req.user._id
   const expense_id = req.params.expense_id
-  await Expense.findOneAndDelete({ _id: expense_id })
+  await Expense.findOneAndDelete({ _id: expense_id, userId })
   // console.log(expense)
   res.redirect('/')
 })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,40 +2,45 @@ const express = require('express')
 const router = express.Router()
 const Expense = require('../../models/expense')
 const Category = require('../../models/category')
-const expense = require('../../models/expense')
 
 router.get('/', async (req, res) => {
   const userId = req.user._id
   const categoryId = req.query.category
   const categories = await Category.find().lean()
+  let totalAmount = 0
 
   if (categoryId) {
-    const category = categories.find(
-      category => category._id.toString() === categoryId
-    )
+    const category = categories.find(category => {
+      if (category._id.toString() === categoryId) {
+        category.selected = 'selected'
+        return category
+      }
+    })
     const { name, icon } = category
     const expenses = await Expense.find({ userId, categoryId })
       .lean()
       .sort({ date: -1 })
 
     expenses.forEach(expense => {
+      totalAmount += Number(expense.amount)
       expense.categoryName = name
       expense.categoryIcon = icon
     })
-    // console.log(expenses)
     res.render('index', { expenses, categories })
   } else {
     const expenses = await Expense.find({ userId }).lean().sort({ date: -1 })
     expenses.forEach(expense => {
+      totalAmount += Number(expense.amount)
       const category = categories.find(
         category =>
           // 切記這裏拿出來的 _id 還是 new ObjectId("6242b51ac976bdf52855e5f8") 的形態 !!!!!!!!!!!!! 要轉成文字才可以用 ！！！
           category._id.toString() === expense.categoryId
       )
-      expense.categoryName = category.name
-      expense.categoryIcon = category.icon
+      const { name, icon } = category
+      expense.categoryName = name
+      expense.categoryIcon = icon
     })
-    res.render('index', { expenses, categories })
+    res.render('index', { expenses, categories, totalAmount })
   }
   // console.log(expenses)
   // res.render('index', { expenses, categories })
