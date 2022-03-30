1515b027fb9792dca7e7f97c790e11da8a014a47
jameshoo123 <hoosinyong@hotmail.com>
2022-03-30 23:58:52 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 require('./config/mongoose')
 
 const hbs = exphbs.create({ defaultLayout: 'main', extname: 'hbs' })
