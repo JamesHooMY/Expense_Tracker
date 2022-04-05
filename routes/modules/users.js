e6c19cc33077ed0fb906f7a049394afc14e66e32
jameshoo123 <hoosinyong@hotmail.com>
2022-04-05 23:08:06 +0800

    feat: change user password function!

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,7 @@ const passport = require('passport')
 const router = express.Router()
 const User = require('../../models/user')
 const bcrypt = require('bcrypt')
+const { authenticator } = require('../../middleware/auth')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -56,6 +57,53 @@ router.post('/register', async (req, res) => {
 router.get('/logout', (req, res) => {
   req.logout()
   req.flash('success_msg', '你已經成功登出。')
+  res.redirect('/users/login')
+})
+
+router.get('/changePassword', authenticator, (req, res) => {
+  res.render('changePassword')
+})
+
+router.put('/changePassword', authenticator, async (req, res) => {
+  const { oldPassword, newPassword, confirmNewPassword } = req.body
+  const errors = []
+  if (oldPassword === newPassword) {
+    errors.push({ message: '新密碼不能與舊密碼相同 !' })
+    return res.render('changePassword', {
+      errors,
+      oldPassword,
+      newPassword,
+      confirmNewPassword,
+    })
+  }
+  if (newPassword !== confirmNewPassword) {
+    errors.push({ message: '新密碼與與新確認密碼不符 !' })
+    return res.render('changePassword', {
+      errors,
+      oldPassword,
+      newPassword,
+      confirmNewPassword,
+    })
+  }
+
+  const userId = req.user._id
+  const user = await User.findOne({ _id: userId })
+  const isMatch = await bcrypt.compare(oldPassword, user.password)
+  if (!isMatch) {
+    errors.push({ message: '舊密碼不正確 !' })
+    return res.render('changePassword', {
+      errors,
+      oldPassword,
+      newPassword,
+      confirmNewPassword,
+    })
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(newPassword, salt)
+  await User.findOneAndUpdate({ _id: userId }, { password: hash })
+  req.logout()
+  req.flash('success_msg', '密碼更換成功，請重新登入後再使用！')
   res.redirect('/users/login')
 })
 
